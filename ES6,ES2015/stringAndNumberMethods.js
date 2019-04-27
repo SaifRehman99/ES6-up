bf0f87c0c564a85213518d5cca8b7c0b4c270ce0
SaifRehman99 <saifr7493@gmail.com>
2019-04-27 13:23:52 +0500

    Adding

--- ES6,ES2015/stringAndNumberMethods.js
+++ ES6,ES2015/stringAndNumberMethods.js
@@ -1,5 +1,16 @@
 let stringThing =  'Hello,this is Javascript';
 
+
+// check the first instance
 console.log(stringThing.startsWith('Hello'));
 
+// check the last instance
 console.log(stringThing.endsWith('Javascript'));
+
+// check the complete instance
+console.log(stringThing.includes('Javascript'));
+
+
+console.log(Number.isFinite(2))
+console.log(Number.isNaN('d'))
+console.log(Number.isInteger(2))
