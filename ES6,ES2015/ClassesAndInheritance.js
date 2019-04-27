a1fc7c09c7a0c0768d6ba02737d85764225336cb
SaifRehman99 <saifr7493@gmail.com>
2019-04-27 12:44:36 +0500

    Inheritance

--- ES6,ES2015/ClassesAndInheritance.js
+++ ES6,ES2015/ClassesAndInheritance.js
@@ -14,7 +14,17 @@ class User {
     register(){
         console.log(`${this.username} is now registered`);
     }
+
+    // we can also create a static method in which no things are instantiated
+    static countUser(){
+        console.log(`There are 100 Users`);
+    }
 }
 
 const user = new User('Saif', 'saifr7493@gmail.com',19);
 console.log(user);
+console.log(user.register());
+console.log(User.countUser())
+
+
+// We can inherit the class as wells
