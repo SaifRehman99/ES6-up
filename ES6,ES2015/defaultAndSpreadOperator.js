4c512c5376b6bb191e9df192c495ad5945079314
SaifRehman99 <saifr7493@gmail.com>
2019-04-27 14:04:51 +0500

    Adding

--- ES6,ES2015/defaultAndSpreadOperator.js
+++ ES6,ES2015/defaultAndSpreadOperator.js
@@ -12,7 +12,25 @@ getNumbers();
 
 // spread operator here
 
+// 1)Adding element in the new array
 var arr = [1,2,3,4];
 var arr1 = [...arr,5,6,7,8];
-console.log(arr1)
+console.log(arr1);
+
+// 2)pass elements as an function arguments
+function add(a,b,c,d){
+    console.log(a+b+c+d);
+}
+add(...arr);
+
+
+// 3)copy the array
+var arr2 = [...arr];
+arr.push(100);
+console.log(arr);
+console.log(arr2);
+
+// 4)concate array
+var arr3 = [...arr,...arr2];
+console.log(arr3)
 // rest operator here
