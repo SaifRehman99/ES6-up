b1a03fae0833b67ed917043a9de6aa2d6c628e79
SaifRehman99 <saifr7493@gmail.com>
2019-04-27 13:48:47 +0500

    ..

--- ES6,ES2015/defaultAndSpreadOperator.js
+++ ES6,ES2015/defaultAndSpreadOperator.js
@@ -3,3 +3,14 @@ function getNumbers(num = 11){
     console.log(num);
 }
 getNumbers();
+
+
+
+// spread operator here
+let arr = [1,2,3,4];
+let arr1 = [1,2,3,4,3];
+
+function okay(){
+    console.log(arr +','+ arr1)
+}
+okay(...arr,arr1)
