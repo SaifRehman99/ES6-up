a13daafdaf98e9ec04c34717c28755fd83aa98be
SaifRehman99 <saifr7493@gmail.com>
2019-04-27 16:05:05 +0500

    added

--- ES6,ES2015/promises.js
+++ ES6,ES2015/promises.js
@@ -7,14 +7,14 @@ myPromise.then((res) => console.log(res));
 
 
 
-let myPromise2 = new Promise((res,rej) =>{
+let myPromise2 = new Promise((res, rej) => {
     setTimeout(() => {
         res(4);
     }, 3000);
 });
 
-myPromise2.then((res) =>{
-    res+=3;
+myPromise2.then((res) => {
+    res += 3;
     console.log(res)
 }).catch((rej) => {
     console.log(rej)
@@ -23,45 +23,45 @@ myPromise2.then((res) =>{
 
 
 
-function getData(method, url){
-    return new Promise(function(resolve, reject){
-      var xhr = new XMLHttpRequest();
-      xhr.open(method, url);
-      xhr.onload = function(){
-        if(this.status >= 200 && this.status < 300){
-          resolve(xhr.response);
-        }else {
-          reject({
-            status: this.status,
-            statusText: xhr.statusText
-          });
-        }
-      };
-      xhr.onerror = function(){
-        reject({
-          status: this.status,
-          statusText: xhr.statusText
-        });
-      };
-      xhr.send();
+function getData(method, url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
     });
-  }
-  
-  getData('GET', 'http://jsonplaceholder.typicode.com/todos').then(function(data){
+}
+
+getData('GET', 'http://jsonplaceholder.typicode.com/todos').then(function (data) {
     let todos = JSON.parse(data);
     let output = '';
-    for(let todo of todos){
-      output += `
+    for (let todo of todos) {
+        output += `
         <li>
           <h3>${todo.title}</h3>
           <p>Completed: ${todo.completed}</p>
         </li>
       `;
     }
-  
+
     document.getElementById('append').innerHTML = output;
-  }).catch(function(err){
+}).catch(function (err) {
     console.log(err);
-  });
+});
 
 console.log('Fetching Data')
