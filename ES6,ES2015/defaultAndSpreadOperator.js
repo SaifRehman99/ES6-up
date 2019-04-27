cbd04a813c55cc2cac1305ca64ea700c08dbd195
SaifRehman99 <saifr7493@gmail.com>
2019-04-27 13:50:18 +0500

    //

--- ES6,ES2015/defaultAndSpreadOperator.js
+++ ES6,ES2015/defaultAndSpreadOperator.js
@@ -6,6 +6,10 @@ getNumbers();
 
 
 
+// The spread operator (...) spreads out the elements of an array (or iterable object). 
+// The rest operator condenses elements. The spread and rest operators are great additions to ES6!
+
+
 // spread operator here
 let arr = [1,2,3,4];
 let arr1 = [1,2,3,4,3];
@@ -14,3 +18,5 @@ function okay(){
     console.log(arr +','+ arr1)
 }
 okay(...arr,arr1)
+
+// rest operator here
