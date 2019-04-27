b23b4e479566bbec57c0478285467400cbeced53
SaifRehman99 <saifr7493@gmail.com>
2019-04-27 14:53:39 +0500

    final touch

--- ES6,ES2015/set,map,weakSet,weakMap.js
+++ ES6,ES2015/set,map,weakSet,weakMap.js
@@ -1,5 +1,6 @@
 // Set stores indivisual values and it stores values/data of diff data types
 
+//Set
 let arr = [1,2,3,4];
 let mySet = new Set(arr);
 mySet.add('name');
@@ -17,6 +18,37 @@ mySet.forEach((item) => {
     console.log(item);
 })
 
-//  Map stores in key value pairs
+// Weak Set (stores values in object form)
+let carWeakSet = new WeakSet();
 
-let myMap = new Map()
+let car1 = {
+    name:'Honda',
+    model:'Corolla'
+}
+carWeakSet.add(car1);
+console.log(carWeakSet)
+
+
+
+
+
+
+
+//  Map stores in key value pairs
+//Map
+let myMap = new Map([['a','10'],['b','20']]);
+myMap.set('c','30');
+myMap.delete('a')
+console.log(myMap);
+
+//WeakMap
+let myWeakMap = new WeakMap();
+
+let ok = {
+    id:1,
+    name:'John',
+    age:10
+}
+
+myWeakMap.set(ok);
+console.log(myWeakMap)
