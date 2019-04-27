22e9b7e574b4a52dab1b4f18a5ac91669bf374c2
SaifRehman99 <saifr7493@gmail.com>
2019-04-27 15:51:45 +0500

    adding

--- ES6,ES2015/promises.js
+++ ES6,ES2015/promises.js
@@ -5,52 +5,63 @@ var myPromise = Promise.resolve('Foo');
 myPromise.then((res) => console.log(res));
 
 
-var myPromise = new Promise(function(resolve, reject){
-  setTimeout(() => resolve(4), 2000);
-});
-myPromise.then((res) => {
-  res +=3;
-  console.log(res);
-});
 
 
-// function getData(method, url){
-//     return new Promise(function(resolve, reject){
-//       var xhr = new XMLHttpRequest();
-//       xhr.open(method, url);
-//       xhr.onload = function(){
-//         if(this.status >= 200 && this.status < 300){
-//           resolve(xhr.response);
-//         }else {
-//           reject({
-//             status: this.status,
-//             statusText: xhr.statusText
-//           });
-//         }
-//       };
-//       xhr.onerror = function(){
-//         reject({
-//           status: this.status,
-//           statusText: xhr.statusText
-//         });
-//       };
-//       xhr.send();
-//     });
-//   }
+let myPromise2 = new Promise((res,rej) =>{
+    setTimeout(() => {
+        res(4);
+    }, 3000);
+});
+
+myPromise2.then((res) =>{
+    res+=3;
+    console.log(res)
+}).catch((rej) => {
+    console.log(rej)
+})
+
+
+
+
+function getData(method, url){
+    return new Promise(function(resolve, reject){
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function(){
+        if(this.status >= 200 && this.status < 300){
+          resolve(xhr.response);
+        }else {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          });
+        }
+      };
+      xhr.onerror = function(){
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      };
+      xhr.send();
+    });
+  }
   
-//   getData('GET', 'http://jsonplaceholder.typicode.com/todos').then(function(data){
-//     let todos = JSON.parse(data);
-//     let output = '';
-//     for(let todo of todos){
-//       output += `
-//         <li>
-//           <h3>${todo.title}</h3>
-//           <p>Completed: ${todo.completed}</p>
-//         </li>
-//       `;
-//     }
+  getData('GET', 'http://jsonplaceholder.typicode.com/todos').then(function(data){
+    let todos = JSON.parse(data);
+    let output = '';
+    for(let todo of todos){
+      output += `
+        <li>
+          <h3>${todo.title}</h3>
+          <p>Completed: ${todo.completed}</p>
+        </li>
+      `;
+    }
   
-//     document.getElementById('template').innerHTML = output;
-//   }).catch(function(err){
-//     console.log(err);
-//   });
+    document.getElementById('append').innerHTML = output;
+  }).catch(function(err){
+    console.log(err);
+  });
+
+console.log('Fetching Data')
